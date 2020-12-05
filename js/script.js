6a57ae80890290bdff9c20a68e11a5f16ee091d6
Beata Zlotkowska <beata.zlotkowska94@gmail.com>
2020-12-05 19:11:23 +0100

    random computer move & player input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
-	let computerMove = 'kamień';
-	let playerMove = 'papier';
-	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomFraction = Math.random();
+let calculation = randomFraction * 3 + 1;
+let roundNumber = Math.floor(calculation);
+
+
+let computerMove = 'nieznany ruch';
+let playerMove = 'nieznany ruch';
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+if (roundNumber == 1) {
+	computerMove = 'kamień';
+
+} else if (roundNumber == 2) {
+	computerMove = 'papier';
+
+} else if (roundNumber == 3) {
+	computerMove = 'nożyce';
+
+} else {
+	computerMove = 'błąd';
+}
+
+
+if (playerInput == 1) {
+	playerMove = 'kamień';
+
+} else if (playerInput == 2) {
+	playerMove = 'papier';
+
+} else if (playerInput == 3) {
+	playerMove = 'nożyce';
+
+} else {
+	playerMove = 'nie ma takiego ruchu xd';
+}
+
+
+
+
+
+console.log(roundNumber);
+console.log(computerMove);
+console.log(playerMove);
