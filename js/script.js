6d971f99aa3d5918e3473fa9ad8df824161f00ee
Beata Zlotkowska <beata.zlotkowska94@gmail.com>
2020-12-05 20:23:44 +0100

    shorten code by using function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,72 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+
+  } else if(argMoveId == 2){
+    return 'papier';
+
+  } else if(argMoveId == 3){
+    return 'nożyce';
+
+  }
+
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+	console.log('moves:', argComputerMove, argPlayerMove);
+
+	if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+		printMessage('Beata wygrała');
+
+	} else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+		printMessage('Beata wygrała');
+
+	} else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+		printMessage('Beata wygrała');
+
+	} else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+		printMessage('Computer wins');
+
+	} else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+		printMessage('Computer wins');
+
+	} else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+		printMessage('Computer wins');
+
+	} else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+		printMessage('Remis');
+
+	} else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
+		printMessage('Remis');
+
+	} else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
+		printMessage('Remis');
+
+	} else if (argPlayerMove == 'nieznany ruch') {
+		printMessage ('Beata oszukuje xd');
+
+	} else {
+		printMessage ('error');
+
+	}
+
+
+}
+
+
 let randomFraction = Math.random();
 let calculation = randomFraction * 3 + 1;
 let roundNumber = Math.floor(calculation);
 
+let computerMove = getMoveName(roundNumber);
 
-let computerMove = 'nieznany ruch';
-let playerMove = 'nieznany ruch';
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let playerMove = getMoveName(playerInput);
 
-if (roundNumber == 1) {
+/*if (roundNumber == 1) {
 	computerMove = 'kamień';
 
 } else if (roundNumber == 2) {
@@ -19,6 +78,7 @@ if (roundNumber == 1) {
 } else {
 	computerMove = 'błąd';
 }
+
 
 
 if (playerInput == 1) {
@@ -33,42 +93,9 @@ if (playerInput == 1) {
 } else {
 	playerMove = 'nieznany ruch';
 }
+*/
 
-
-if (computerMove == 'kamień' && playerMove == 'papier') {
-	printMessage('Beata wygrała');
-
-} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-	printMessage('Beata wygrała');
-
-} else if (computerMove == 'papier' && playerMove == 'nożyce') {
-	printMessage('Beata wygrała');
-
-} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-	printMessage('Computer wins');
-
-} else if (computerMove == 'papier' && playerMove == 'kamień') {
-	printMessage('Computer wins');
-
-} else if (computerMove == 'nożyce' && playerMove == 'papier') {
-	printMessage('Computer wins');
-
-} else if (computerMove == 'kamień' && playerMove == 'kamień') {
-	printMessage('Remis');
-
-} else if (computerMove == 'papier' && playerMove == 'papier') {
-	printMessage('Remis');
-
-} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
-	printMessage('Remis');
-
-} else if (playerMove == 'nieznany ruch') {
-	printMessage ('Beata oszukuje xd');
-
-} else {
-	printMessage ('error');
-
-}
+displayResult(computerMove, playerMove);
 
 
 console.log('ruch komputera '+computerMove);
